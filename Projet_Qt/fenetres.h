8b336dd2b738b86483cef0fe7ac621e8b35d2fc6
Cyril <cyril31180@hotmail.fr>
2017-06-15 14:47:05 +0200

    début doc fenetreprincipale

--- Projet_Qt/fenetres.h
+++ Projet_Qt/fenetres.h
@@ -9,6 +9,13 @@
 /* *********** FENETRE PRINCIPALE ************* */
 
 class fenetre_anciennes_versions;
+
+/// Classe Fenetre principale
+/** Cette fenêtre va servir à visualiser tous les différents objets : liste des notes, liste des taches selon leur priorité,
+ * liste des relations. Elle contient aussi un Widget central contenant trois onglets. Ce widget central permet l'affichage de l'arborescence,
+ * ou de la note/relation sélectionnée dans le dock adéquat.
+ *
+ */
 class fenetre_creation_relation;
 
 class FenPrincipale : public QMainWindow
@@ -16,8 +23,10 @@ class FenPrincipale : public QMainWindow
     Q_OBJECT
 
 protected:
-    void creation_docks(); //Création du dock de gauche, et affichage des notes
-    void creation_tabs(); //Création des différents onglets de la vue principale
+    /// fonction qui initialise les docks, et réalise les connexions nécéssaires aux affichages, ainsi qu'au menu contextuel.
+    void creation_docks();
+    /// fonction qui initialise les onglets et leurs layouts
+    void creation_tabs();
 
     QToolBar* m_toolbar; //barre d'outils
 
@@ -62,24 +71,52 @@ protected:
 
 public:
 
+    ///Initialise tous les widgets de la fenetre principale. Fait appel à creation_docks et creation_tabs.
     FenPrincipale();
+    ///Retourne l'id de la note actuellement sélectionnée dans le dock.
     const std::string getCurrentNote() const {return m_listeNotes->currentItem()->text().toStdString();}
+    ///Retourne le titre de la relation actuellement sélectionnée dans le dock.
     const std::string getCurrentRelation() const {return m_listeRelatons->currentItem()->text().toStdString();}
 
 public slots:
+    ///Affiche la fenetre modale de création de note
     void popup();
+    ///Affiche une fenetre modale qui contient la liste des versions de la note sélectionnée, permet d'en restaurer une.
     void popupAnciennesVersions();
+    ///Affiche la fenetre modale de création de relation
     void popupCreationRelation();
+    ///Remplit les différents docks avec les objets du manager
     void affichage_notes_relations();
+    ///Affiche au centre de la fenetre, la note sélectionnée dans le dock
+    /**
+    * @param id : identifiant de la note, transmis via des signaux
+    * @param date : date correspondante à la version recherchée. Si la date est non renseignée, c'est la note active qui est recherchée.
+    */
     void affichage_single_note(QString id, QString date="");
+    ///Affiche la relation sélectionnée dans son dock
+    /**
+     * @param titre : titre de la relation recherchée, envoyé via un signal depuis un dock
+     */
     void affichage_single_relation(QString titre);
+
+    ///Menu contextuel personnalisé permettant des opérations d'édition, de suppression ou de restauration d'ancienne version sur une note
     void menuContextuel(const QPoint&);
+    ///Menu contextuel personnalisé permettant des opérations d'édition ou de suppression sur une relation
     void menuContextuel2(const QPoint&);
+    ///Supprime la note sélectionnée dans son dock
     void supprimerNote();
+    ///Supprime la relation sélectionnée dans son dock
     void supprimerRelation();
+    ///Appelle la fenetre de création de note, mais en mode "edition" : l'id et le type ne sont plus modifiables
     void editerNote();
+    ///Appelle la fenetre de création de relation, en mode édition: le titre n'est plus modifiable.
     void editerRelation();
+    ///Crée tous les objets contenus dans le xml "sauvegarde.xml"
     void load_xml();
+    ///Affiche les ascendants et descendants d'une note
+    /**
+     * @param id : identifiant de la note, transmis via un signal
+     */
     void affichage_arborescence(QString id);
 
 
